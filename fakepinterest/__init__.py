249e1ebe6b7773eb3310b64aa2a0f21ead48f5ce
Leonardo <leo.nardo.360@hotmail.com>
2024-02-23 20:03:46 -0300

    Atualizar fotos

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -8,7 +8,7 @@ import os
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "28d1f70beb7d77db326809d591b43412"
-app.config["UPLOAD_FOLDER"] = "/fotos_posts"
+app.config["UPLOAD_FOLDER"] = "/fotos_post"
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
