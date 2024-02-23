f8b1a5d800b744bb2dcf5c3e580d081bc7ca46ba
Leonardo <leo.nardo.360@hotmail.com>
2024-02-23 17:22:41 -0300

    change-db

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 # Init.file
 app = Flask(__name__)
